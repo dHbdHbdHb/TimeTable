acb1b54a8f4be865139d306a5cf0d847c90a32bc
dHbdHbdHb <108880035+dhbSF@users.noreply.github.com>
2023-02-28 19:49:52 -0800

    Prevent caching of the image

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import time
 import os
 
 app=Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 image_folder = os.path.join('static', 'images')
 
 @app.route('/')
@@ -12,6 +13,14 @@ def index():
 @app.route('/images/<path:path>')
 def serve_image(path):
     return send_from_directory(image_folder, 'display_table.png')
+
+# No caching at all for API endpoints.
+@app.after_request
+def add_header(response):
+    # response.cache_control.no_store = True
+    if 'Cache-Control' not in response.headers:
+        response.headers['Cache-Control'] = 'no-store'
+    return response
 
 if __name__== '__main__':
     app.run(host='0.0.0.0',debug=True)
