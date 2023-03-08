3d1a3de2850f97d27e4811be51765949f443fe97
dHbdHbdHb <108880035+dhbSF@users.noreply.github.com>
2023-03-08 10:39:27 -0800

    Smarter color text so stop specific depart tags

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from PIL import Image, ImageDraw, ImageFont
 import os, glob
 import threading
 import time
-# Set up the GPIO pin
+Set up the GPIO pin
 import RPi.GPIO as GPIO
 button_pin = 2
 GPIO.setmode(GPIO.BCM)
@@ -19,16 +19,15 @@ GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 api_key = 'da56733a-6d62-4da1-9d2f-4e882d46478b'
 
 stops_dict = {
-    'portola' : '16121', # Portola Dr & Teresita Blvd
-    'lh' : '15255', #Laguna Honda & Ulloa
-    'lh_to_hp' : '15254', # 43 going the other way
-    'oshaughn' : '15834', # 44 going the other way
-    'portola_lh' : '16113',
-    'woodside' : '16937', #Woodside towards Forest Hill
-    'woodsied_to_gp' : '16938', #Woodside towards Glen Park
-    'evelyn' : '16669', #Evelyn Way towards Forest Hill
-    'evelyn_to_gp' : '16665' #Evelyn Way towards Glen Park/Mission
-
+    '16121' : 5, # Portola Dr & Teresita Blvd
+    '15255' : 5, #Laguna Honda & Ulloa
+    '15254' : 4, # 43 going the other way
+    '15834' : 6, # 44 going the other way
+    '16113' : 5, #48 towards ocean
+    '16937' : 7, #Woodside towards Forest Hill
+    '16938' : 7, #Woodside towards Glen Park
+    '16669' : 3, #Evelyn Way towards Forest Hill
+    '16665' : 2 #Evelyn Way towards Glen Park/Mission
 }
 
 routes = ['36','36','43', '43', '44', '44', '48','48', '52', '52', 'LBUS','LBUS']
@@ -142,29 +141,49 @@ def format_time(df, row, col):
     time = datetime.strptime(re.subn('[T]', ' ', df.at[row,col])[0].replace('Z',''),'%Y-%m-%d %H:%M:%S')
     return time
 
+def color_tag(val, stop_id):
+    green_time = 8
+    red_time = 2
+    walking_time = stops_dict.get(stop_id) #stops_dict["monitored_stop_id"]
+    val = format_time_delta(val)
+    etd = int(val.split()[0])
+    if (etd - walking_time) >= green_time:
+        return val + ":B"
+        #append B?
+    elif (etd - walking_time) < green_time and (etd - walking_time) > red_time:
+        return val + ":G"
+        #append G
+    elif (etd - walking_time) <= red_time:
+        return val + ":R"
+        #append R
+    else:
+        return val + "error"
+
 def filter_by_time(df):
+    #filter out all 36s at stop codes 16938 and 16937
     df = df.loc[(df['line'] != '36') | ((df['line'] == '36') & (df['monitored_stop_id'].isin(['16669', '16665'])))].reset_index(drop = True)
     delta = []
     for i in range(df.shape[0]):
         #Some reformatting of objs/strings in df to be in datetime format
         timestamp = format_time(df, 0, 'timestamp')
         first_arrival = format_time(df, i, 'expected_arrival_time')
-        first_line = df.at[i,'line']
-        direction = df.at[i, 'destination_stop_name']
         delta.append(first_arrival - timestamp)
     df["time_till_departure"] = delta
-    twentyfive = timedelta(minutes = 60)
+    df["time_till_departure"] = df.apply(lambda x: color_tag(x['time_till_departure'], x['monitored_stop_id']), axis = 1)
     df.sort_values(['line','destination_stop_name'], inplace=True, ignore_index=True)
     return df.reset_index(drop = True)
-    #return df.loc[df['time_till_departure'] < twentyfive].reset_index(drop = True)
 
 def relevant_format(df):
     df = filter_by_time(df)
-    pivot_df = pd.pivot_table(df, values = "time_till_departure", index=["line","destination_stop_name"], columns=df.groupby(['line',"destination_stop_name"]).cumcount())
+    pivot_df = pd.pivot_table(df, 
+                              values = "time_till_departure", 
+                              index=["line","destination_stop_name"], 
+                              columns=df.groupby(['line',"destination_stop_name"]).cumcount(),
+                              aggfunc=lambda x: ' '.join(x)
+                              )
     pivot_df.columns = [f"Arrival {i}" for i in range(1, len(pivot_df.columns)+1)]
     pivot_df = pivot_df.reset_index()
     pivot_df = pivot_df.rename(columns={'line': 'Route', 'destination_stop_name': 'Destination', 'Arrival 1': 'Next Arrival'})
-    pivot_df = pivot_df.applymap(lambda x: format_time_delta(x) if isinstance(x, timedelta) else x)
     pivot_df = pivot_df.fillna("No Next Arrival")
     pivot_df = pivot_df.replace({"Bayview District - Hudson & Newhall": 'Bayview', 'Wawona + 46th Avenue': 'Wawona +46th Ave'})
     routes = pivot_df['Route'].tolist()
@@ -175,13 +194,13 @@ def relevant_format(df):
     clean_df = pd.concat([clean_df, empty_cols], axis=1)
     clean_df[['Next Arrival', '2nd Arrival', '3rd Arrival']] = \
         pivot_df.loc[:, ~pivot_df.columns.isin(['Route', 'Destination'])].iloc[:, :3].values
-    print(clean_df)
     return clean_df
 
 def make_image(df):
     #Set the fonts and header size
     font_size = 18
     font= ImageFont.truetype('fonts/DIN Alternate Bold.ttf', font_size)
+    font_NA= ImageFont.truetype('fonts/DIN Alternate Regular.ttf', font_size)
     font_header = ImageFont.truetype('fonts/SFCompact.ttf', 20)
     font_route = ImageFont.truetype('fonts/SFCompact.ttf', 35)
     font_single_route = ImageFont.truetype('fonts/SFCompact.ttf', 30)
@@ -217,13 +236,13 @@ def make_image(df):
                     draw.rectangle((x, y + cell_height , x + cell_width, y + (cell_height*2)), fill=(211, 211, 211), outline= (0,0,0))
                 #condition to change color based off whether should leave or not
                 if j >= 2 and val.split()[0] == "No":
-                    draw.text((x + 3, y + cell_height+ padding), str(val), font=font, fill=(0, 0, 0))
-                elif j >= 2 and int(val.split()[0]) >= 4 and int(val.split()[0]) <= 15:
-                    draw.text((x + 3, y + cell_height + padding), str(val), font=font, fill=(10, 110, 10))
-                elif j >= 2 and int(val.split()[0]) <= 4:
-                    draw.text((x + 3, y + cell_height + padding), str(val), font=font, fill=(255, 25, 37))
+                    draw.text((x + 3, y + cell_height+ padding), val, font=font_NA, fill='black') 
+                elif j >= 2 and val.split(":")[1] == "G": #and int(val.split()[0]) >= 4 and int(val.split()[0]) <= 15: ### something like str(val.split(":")[1] == G
+                    draw.text((x + 3, y + cell_height + padding), val.split(":")[0], font=font, fill=(10, 110, 10)) #Write in green text
+                elif j >= 2 and val.split(":")[1] == "R": #int(val.split()[0]) <= 4: ### something like str(val).split(":")[1] == R
+                    draw.text((x + 3, y + cell_height + padding), val.split(":")[0], font=font, fill=(255, 25, 37))
                 else:
-                    draw.text((x + 3, y + cell_height + padding), str(val), font=font, fill=(0, 0, 0))
+                    draw.text((x + 3, y + cell_height + padding), val.split(":")[0], font=font, fill='black')
             elif (df['Route'] == val).sum() == 1: #The first column if there is a single instance of the route
                 draw.rectangle((x, y, x + cell_width, y + cell_height), fill=(150, 94, 209), outline=(0, 0, 0))
                 draw.text((x + 15, y + padding), str(val), font=font_single_route, fill=(0, 0, 0))
@@ -251,6 +270,8 @@ def make_image(df):
     # return the image
     return image
 
+
+# Comment this out for debugg
 try:    
     while(True):
         api = api_511(api_key)
@@ -263,3 +284,13 @@ try:
 except KeyboardInterrupt:
     GPIO.cleanup()
 
+
+# Comment this out if on Pi
+# while(True):
+#     api = api_511(api_key)
+#     local_df = pd.DataFrame()
+#     for key in stops_dict:
+#         local_df = pd.concat([local_df, api.get_stop_monitoring(key)], ignore_index=True)
+#     relevant_df = relevant_format(local_df)
+#     make_image(relevant_df)
+#     time.sleep(100)
